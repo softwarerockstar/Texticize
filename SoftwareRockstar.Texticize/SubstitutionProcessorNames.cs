efe72aed1c5b013b66c25ce6da1740fdbd3efe26
SoftwareRockstar_cp <SND\SoftwareRockstar_cp>
2011-06-27 21:03:28 +0000

    Renamed folders

---
+++ SoftwareRockstar.Texticize/SubstitutionProcessorNames.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright author="Muhammad Haroon">
+//      Texticize
+//      Codeplex Project: http://texticize.codeplex.com/
+//      Copyright (c) Muhammad Haroon, http://www.softwarerockstar.com/
+//      Released under Apache License Version 2.0, http://www.apache.org/licenses/      
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Vocalsoft.Texticize
+{
+    public static class SubstitutionProcessorNames
+    {        
+        public const string Macro = "Vocalsoft.Texticize.Processors.MacroProcessor";
+        public const string Vocabulary = "Vocalsoft.Texticize.Processors.VocabularyProcessor";
+    }
+}
